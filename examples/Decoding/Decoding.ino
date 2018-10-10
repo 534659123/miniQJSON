05d326bac9b8c73c0ed8fb815e2e8c8fcc7a9a7e
迷你强 <chenglei1234@gmail.com>
2018-10-10 16:30:49 +0800

    修正历程中的函数名错误

--- examples/Decoding/Decoding.ino
+++ examples/Decoding/Decoding.ino
@@ -7,11 +7,11 @@ String data = "{\"O\":1,\"D\":[32.20,65.70],\"F\":\"arduinoNode\",\"T\":\"IotSer
 void setup() 
 {
   Serial.begin(9600);  // 初始化串口
-  miniQ.DateBuffing(data);  //将字符串传入
+  miniQ.dataBuffing(data);  //将字符串传入
   uint16_t O =  miniQ.oper();   //返回指令中的“O”的值，操作指令值
   Serial.print("O:");
   Serial.println(O);  //发送编码完成的缓冲区数据
-  uint16_t DataLength = miniQ.DateSizeNum(); //传输数组的时候需要获取数组长度，如果是单个数据则不需要
+  uint16_t DataLength = miniQ.dataSizeNum(); //传输数组的时候需要获取数组长度，如果是单个数据则不需要
   float data[DataLength];  //创建缓冲区
   miniQ.Decoding(data);
   Serial.print("D:");
